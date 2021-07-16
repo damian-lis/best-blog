d9c0cce122b9e2d0f6de67d7c1c57198dc37305f
Damian Lis <foxprogramming93@gmail.com>
2021-07-16 14:24:20 +0200

    Configured babel and webpack based on downloaded packages

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['@babel/transform-runtime'],
+        },
       },
       {
         test: /\.(jpg|png|svg)$/,
