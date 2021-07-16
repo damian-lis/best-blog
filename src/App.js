d6a7b322c464690199c84ed8c0c313efc7bfdb87
Damian Lis <foxprogramming93@gmail.com>
2021-07-16 14:32:11 +0200

    Created basic structure in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Home from './pages/Home';
+import Article from './pages/Article';
 
 const App = () => {
-  return <div className='app'>Hello world!</div>;
+  return (
+    <Router>
+      <div className='app'>
+        <Navigation />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/article/:id' component={Article} />
+          <Route component={() => 404} />
+        </Switch>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
