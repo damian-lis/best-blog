c8f9f63a18f9db20e0b15391eed7e3d7cad150fc
Damian Lis <foxprogramming93@gmail.com>
2021-07-16 23:20:44 +0200

    Added new comments reduer related to favorite comments

--- src/reducers/comments.reducers.js
+++ src/reducers/comments.reducers.js
@@ -5,6 +5,8 @@ import {
   GET_ARTICLE_COMMENTS_REQUEST,
   GET_ARTICLE_COMMENTS_SUCCESS,
   GET_ARTICLE_COMMENTS_FAIL,
+  ADD_FAVORITE_COMMENT,
+  REMOVE_FAVORITE_COMMENT,
 } from '../constants/comments.constants';
 
 export const commentsReducer = (state = [], action) => {
@@ -42,6 +44,32 @@ export const articleCommentsReducer = (state = [], action) => {
       };
     case GET_ARTICLE_COMMENTS_FAIL:
       return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const favoriteCommentsReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_FAVORITE_COMMENT:
+      const commentExist = state.favoriteComments.some(
+        (favoriteComment) => favoriteComment.id === action.payload.id
+      );
+
+      return {
+        ...state,
+        favoriteComments: !commentExist
+          ? [...state.favoriteComments, action.payload]
+          : state.favoriteComments,
+      };
+    case REMOVE_FAVORITE_COMMENT:
+      return {
+        ...state,
+        favoriteComments: state.favoriteComments.filter(
+          (comment) => comment.id !== action.payload.id
+        ),
+      };
+
     default:
       return state;
   }
