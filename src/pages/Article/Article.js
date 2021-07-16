6d95dc28c1784f132158c352337c400684b2d4ee
Damian Lis <foxprogramming93@gmail.com>
2021-07-16 23:25:04 +0200

    Added the ability to add and delete favorite comments and posts in the Article page

--- src/pages/Article/Article.js
+++ src/pages/Article/Article.js
@@ -1,10 +1,18 @@
 import React, { useEffect } from 'react';
-import { getPost } from '../../actions/posts.actions';
-import { getArticleComments } from '../../actions/comments.actions';
+import {
+  getPost,
+  addFavoritePost,
+  removeFavoritePost,
+} from '../../actions/posts.actions';
+import {
+  getArticleComments,
+  addFavoriteComment,
+  removeFavoriteComment,
+} from '../../actions/comments.actions';
 import { useDispatch, useSelector } from 'react-redux';
 
 const Article = ({ match, history }) => {
-  const id = match.params.id;
+  const id = Number(match.params.id);
 
   console.log(history);
 
@@ -19,6 +27,29 @@ const Article = ({ match, history }) => {
     error: errorLoading,
   } = useSelector((state) => state.articleCommentsState);
 
+  const { favoritePosts } = useSelector((state) => state.favoritePostsState);
+  const { favoriteComments } = useSelector(
+    (state) => state.favoriteCommentsState
+  );
+
+  const postIsLiked = favoritePosts.some(
+    (favoritePost) => favoritePost.id === post.id
+  );
+
+  const toggleFavoritePost = () => {
+    dispatch(
+      postIsLiked ? removeFavoritePost({ post }) : addFavoritePost({ post })
+    );
+  };
+
+  const toggleFavoriteComment = (commentIsLiked, articleComment) => {
+    dispatch(
+      commentIsLiked
+        ? removeFavoriteComment({ comment: articleComment })
+        : addFavoriteComment({ comment: articleComment })
+    );
+  };
+
   useEffect(() => {
     dispatch(getPost({ id }));
     dispatch(getArticleComments({ id }));
@@ -28,14 +59,28 @@ const Article = ({ match, history }) => {
     <p>Loading</p>
   ) : (
     <div>
-      <h2>{post.title}</h2>
+      <h2 onClick={toggleFavoritePost}>
+        {post.title} {postIsLiked && 'Lubisz to!'}
+      </h2>
       <p style={{ backgroundColor: 'red' }}>{post.body}</p>
-      {articleComments.map((articleComment) => (
-        <>
-          <p>{articleComment.email}</p>
-          <p>{articleComment.body}</p>
-        </>
-      ))}
+      {articleComments.map((articleComment, index) => {
+        const commentIsLiked = favoriteComments.some(
+          (favoriteComment) => favoriteComment.id === articleComment.id
+        );
+
+        return (
+          <div
+            onClick={() =>
+              toggleFavoriteComment(commentIsLiked, articleComment)
+            }
+            key={index}
+          >
+            <p>{articleComment.email}</p>
+            <p>{articleComment.body}</p>
+            <p>{commentIsLiked && 'Lubisz to!'}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
