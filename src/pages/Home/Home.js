1a5f34f56d219fac4d50e0a34326621ba844c256
Damian Lis <foxprogramming93@gmail.com>
2021-07-17 20:21:06 +0200

    Added the ability to display the appropriate number of posts and its dynamic change

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPosts } from '../../actions/posts.actions';
 import { getComments } from '../../actions/comments.actions';
@@ -7,7 +7,12 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   const dispatch = useDispatch();
 
+  const [articlesNumber, setArticlesNumber] = useState(
+    Number(localStorage.getItem('postsNumber')) || 10
+  );
+
   let { posts, loading, error } = useSelector((state) => state.postsState);
+  const postsLength = posts.length;
   const {
     comments,
     loading: commentsLoading,
@@ -22,6 +27,47 @@ const Home = () => {
   posts = posts.filter((post) =>
     post.title.toLowerCase().includes(searchPosts)
   );
+
+  posts = posts.slice(0, articlesNumber);
+
+  const handleArticlesNumber = (action) => {
+    let range = 6;
+
+    switch (action) {
+      case 'add':
+        const canAdd = articlesNumber + range < postsLength;
+        console.log(canAdd);
+
+        if (!canAdd) {
+          range = postsLength - articlesNumber;
+        }
+
+        localStorage.setItem(
+          'postsNumber',
+          JSON.stringify(articlesNumber + range)
+        );
+
+        setArticlesNumber(articlesNumber + range);
+        break;
+      case 'subtract':
+        const canSubtract = articlesNumber - range > 0;
+
+        if (!canSubtract) {
+          range = posts.length;
+        }
+
+        localStorage.setItem(
+          'postsNumber',
+          JSON.stringify(articlesNumber - range)
+        );
+
+        setArticlesNumber(articlesNumber - range);
+        break;
+
+      default:
+        break;
+    }
+  };
 
   useEffect(() => {
     dispatch(getPosts());
@@ -63,6 +109,18 @@ const Home = () => {
           );
         })}
       </div>
+      <button
+        disabled={articlesNumber === postsLength}
+        onClick={() => handleArticlesNumber('add')}
+      >
+        Zobacz X następnych postów!
+      </button>
+      <button
+        disabled={articlesNumber === 0}
+        onClick={() => handleArticlesNumber('subtract')}
+      >
+        Schowaj X ostatnich postów!
+      </button>
     </section>
   );
 };
