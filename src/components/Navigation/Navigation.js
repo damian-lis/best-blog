10025b0bcbe3602486b88c919e5154b8455de89a
Damian Lis <foxprogramming93@gmail.com>
2021-07-17 09:11:58 +0200

    Added an option to display the number of likes in the Navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import './navigation.css';
 
-const navLinks = [{ title: 'Home', path: '/' }];
+const navLinks = [
+  { title: 'Home', path: '/' },
+  { title: 'Favorites', path: '/favorites' },
+];
 
 const Navigation = () => {
+  const { favoritePosts } = useSelector((state) => state.favoritePostsState);
+  const { favoriteComments } = useSelector(
+    (state) => state.favoriteCommentsState
+  );
+
+  const favoritesNumber = favoritePosts.length + favoriteComments.length;
+
   return (
     <nav className='navigation'>
       <h3 className='navigation__logo'>Best Blog</h3>
       <ul className='navigation__list'>
         {navLinks.map((navLink) => (
           <li className='navigation__item' key={navLink}>
-            <Link to={navLink.path}> {navLink.title}</Link>
+            <Link to={navLink.path}>
+              {navLink.title === 'Favorites'
+                ? navLink.title + ` (${favoritesNumber})`
+                : navLink.title}
+            </Link>
           </li>
         ))}
       </ul>
