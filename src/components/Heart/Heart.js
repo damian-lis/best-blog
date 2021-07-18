eb707974675cf1c1d482d9872e0f3f37d83077ec
Damian Lis <foxprogramming93@gmail.com>
2021-07-18 15:54:21 +0200

    Added the ability to toggle icons in the Heart component

--- src/components/Heart/Heart.js
+++ src/components/Heart/Heart.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import heartIconImg from '../../assets/heartIcon.svg';
+import heartIcon from '../../assets/heartIcon.svg';
+import grayHeartIcon from '../../assets/grayHeartIcon.svg';
 import './heart.css';
 
-const Heart = ({ number }) => {
+const Heart = ({ number, toggle = true }) => {
   return (
     <div className={'heart'}>
-      <img className={'heart__icon'} src={heartIconImg} />
+      <img className={'heart__icon'} src={toggle ? heartIcon : grayHeartIcon} />
       <span className={'heart__number'}>{number}</span>
     </div>
   );
