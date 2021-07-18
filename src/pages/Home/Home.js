07c4d8cdb00a090d594a9577773d3d7e84e4aa5d
Damian Lis <foxprogramming93@gmail.com>
2021-07-18 14:06:00 +0200

    Modified the logic structure of the Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPosts } from '../../actions/posts.actions';
 import { getComments } from '../../actions/comments.actions';
-import { Link } from 'react-router-dom';
+
+import Container from '../../components/Container';
+import SneakPeek from '../../components/SneakPeek';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -75,53 +77,33 @@ const Home = () => {
   }, [dispatch]);
 
   return (
-    <section className='container'>
-      <div className='row'>
-        <h2>Trending Posts</h2>
-        {posts.map((post) => {
-          const commentsMatching = comments.filter(
-            (comment) => comment.postId === post.id
-          );
-
-          const postLiked = favoritePosts.some(
-            (favoritePost) => favoritePost.id === post.id
-          );
-
-          const commentsLiked = favoriteComments.filter(
-            (comment) => comment.postId === post.id
-          );
-
-          return (
-            <div key={post.id}>
-              <h3>
-                <Link to={`article/${post.id}`}>
-                  {post.title} {postLiked && 'Polubiony'}
-                </Link>
-              </h3>
-              <p>{post.body}</p>
-              {commentsMatching && (
-                <>
-                  <span>Comments: {commentsMatching.length}</span>
-                  <span>Liked Comments: {commentsLiked.length}</span>
-                </>
-              )}
-            </div>
-          );
-        })}
+    <>
+      <Container home>
+        {posts.map((post) => (
+          <SneakPeek
+            key={post.id}
+            post={post}
+            comments={comments}
+            favoriteComments={favoriteComments}
+            favoritePosts={favoritePosts}
+          />
+        ))}
+      </Container>
+      <div>
+        <button
+          disabled={articlesNumber === postsLength}
+          onClick={() => handleArticlesNumber('add')}
+        >
+          Zobacz X następnych postów!
+        </button>
+        <button
+          disabled={articlesNumber === 0}
+          onClick={() => handleArticlesNumber('subtract')}
+        >
+          Schowaj X ostatnich postów!
+        </button>
       </div>
-      <button
-        disabled={articlesNumber === postsLength}
-        onClick={() => handleArticlesNumber('add')}
-      >
-        Zobacz X następnych postów!
-      </button>
-      <button
-        disabled={articlesNumber === 0}
-        onClick={() => handleArticlesNumber('subtract')}
-      >
-        Schowaj X ostatnich postów!
-      </button>
-    </section>
+    </>
   );
 };
 
