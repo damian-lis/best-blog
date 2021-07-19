048e8599429485c925e489399c3c47f11b3d565b
Damian Lis <foxprogramming93@gmail.com>
2021-07-19 13:03:30 +0200

    Customized the Favorites page for Comment and SneakPeek components

--- src/pages/Favorites/Favorites.js
+++ src/pages/Favorites/Favorites.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+
 import { removeFavoritePost } from '../../actions/posts.actions';
 import { removeFavoriteComment } from '../../actions/comments.actions';
 import SearchBar from '../../components/SearchBar';
+import Comment from '../../components/Comment';
+import SneakPeek from '../../components/SneakPeek';
+import Container from '../../components/Container';
+import './favorites.css';
 
 const Favorites = () => {
   let { favoritePosts } = useSelector((state) => state.favoritePostsState);
@@ -34,30 +38,35 @@ const Favorites = () => {
 
   return (
     <div>
-      <h3>Ulubione posty</h3>
-      <SearchBar posts />
-      {favoritePosts.map((favoritePost, index) => (
-        <div key={index} style={{ margin: 20 }}>
-          <p>{favoritePost.title}</p>
-          <p>{favoritePost.name}</p>
-          <p>{favoritePost.body}</p>
-          <p onClick={() => handleRemoveFavoritePost(favoritePost)}>Usuń</p>
-          <Link to={`article/${favoritePost.id}`}>Przejdź do tego postu</Link>
-        </div>
-      ))}
-      <h3>Ulubione komentarze</h3>
-      <SearchBar comments />
+      <h3 className='favorites__header'>
+        Ulubione posty ({favoritePosts.length})
+      </h3>
+      <div className='favorites__search-bar-container'>
+        <SearchBar posts />
+      </div>
+      <Container>
+        {favoritePosts.map((post) => (
+          <SneakPeek
+            favorites
+            key={post.id}
+            post={post}
+            handleRemove={handleRemoveFavoritePost}
+          />
+        ))}
+      </Container>
+      <h3 className='favorites__header'>
+        Ulubione komentarze ({favoriteComments.length})
+      </h3>
+      <div className='favorites__search-bar-container'>
+        <SearchBar comments />
+      </div>
       {favoriteComments.map((favoriteComment, index) => (
-        <div key={index} style={{ margin: 20 }}>
-          <p>{favoriteComment.email}</p>
-          <p>{favoriteComment.body}</p>
-          <p onClick={() => handleRemoveFavoriteComment(favoriteComment)}>
-            Usuń
-          </p>
-          <Link to={`article/${favoriteComment.postId}`}>
-            Przejdź do postu z tym komentarzem
-          </Link>
-        </div>
+        <Comment
+          key={index}
+          comment={favoriteComment}
+          handleRemove={handleRemoveFavoriteComment}
+          favorite
+        />
       ))}
     </div>
   );
