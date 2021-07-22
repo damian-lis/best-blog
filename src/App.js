6676239cba344f07854cf8ad45faf43d9fa39f2b
Damian Lis <foxprogramming93@gmail.com>
2021-07-22 09:25:41 +0200

    Added main app styles to the Container component and used their in the App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Footer } from './components';
+import { Footer, Container } from './components';
 import { Navigation } from './containers';
 import { Home, Article, Favorites } from './pages';
 
 const App = ({ ...restProps }) => {
   return (
-    <div {...restProps} className="app">
+    <Container main {...restProps}>
       <Router>
         <Navigation />
-        <div className="app__wrapper">
+        <Container mainWrapper>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/favorites" component={Favorites} />
             <Route exact path="/article/:id" component={Article} />
             <Route component={() => 404} />
           </Switch>
-        </div>
+        </Container>
         <Footer />
       </Router>
-    </div>
+    </Container>
   );
 };
 
--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './container.module.css';
 
-const Container = ({ children, wrap, column, base, ...restProps }) => {
+const Container = ({ children, wrap, column, base, main, mainWrapper, ...restProps }) => {
   return (
     <div
       {...restProps}
       className={`${styles.container} ${wrap ? styles[`container--wrap`] : ''}
     ${column ? styles[`container--column`] : ''}
     ${base ? styles[`container--base`] : ''}
+    ${main ? styles[`container--main`] : ''}
+    ${mainWrapper ? styles[`container--mainWrapper`] : ''}
     `}>
       {children}
     </div>
